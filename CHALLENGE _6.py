f92392444ad5f1b1e312b1f3a9a7837466f7af43
Shua <syuranwar@gmail.com>
2024-12-22 10:13:16 +0800

    CHALLENGE _6.py

--- CHALLENGE _6.py
+++ CHALLENGE _6.py
@@ -50,6 +50,32 @@ else:
 #-Add course
 #-Remove course
 #-View
+def start_menu():
+    login_admin_student(signin)
+    if studentID == currentID:
+        menu = input('Menu\n1.Register Course\n2.Drop Courses\n3.Enrollment Status')
+        if menu == 1:
+            subjects = [
+                {'code': 'ML119', 'name': 'Machine Learning', 'credits': 4},
+                {'code': 'CS102', 'name': 'Python Programming', 'credits': 4},
+                {'code': 'DM111', 'name': 'Discrete Math', 'credits': 3},
+                {'code': 'DAT101', 'name': 'Data Management', 'credits': 3},
+                {'code': 'IA101', 'name': 'Introduction to AI', 'credits': 2},
+            ]
+            print('Available Subjects:')
+            for subjects in subjects:
+                print(f'{subjects["code"]}: {subjects["name"]} ({subjects["credits"]} credits)')
+            
+            print('\nEnroll in subjects by entering their codes (type "done" when finished):')
+            enrolled_subjects =[]
+            while True:
+                code = input("Enter Subject Code :").upper()
+                if code in subjects:
+                    enrolled_subjects.append(code)
+                elif code == 'done':
+                    break
+                else:
+                    print('Invalid code. Please try again')
 
 #Admin
 #-Handle course
