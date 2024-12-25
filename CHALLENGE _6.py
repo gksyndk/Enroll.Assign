0c35e164241aca736efa825315b8896a9ffe2673
hazieq22 <126129623+hazieq22@users.noreply.github.com>
2024-12-25 23:28:05 +0800

    CHALLENGE _6.py

--- CHALLENGE _6.py
+++ CHALLENGE _6.py
@@ -1,55 +1,6 @@
-#2 users
-#-Student
-#-Admin
-
-def login_admin_student(signin):
-    if signin == 1:  # Student login
-        student = input("Enter student ID: ").strip()
-        if student in studentID:
-            print("Welcome, Student:", student)
-            return 'student', student
-        else:
-            print("Invalid student ID. Please try again.")
-            return None, None
-
-    elif signin == 2:  # Admin login
-        admin = input("Enter admin ID: ").strip()
-        if admin == adminID:
-            print("Welcome, Admin:", admin)
-            return 'admin', admin
-        else:
-            print("Invalid admin ID. Please try again.")
-            return None, None
-
-    else:
-        print("Invalid option. Please select 1 for Student or 2 for Admin.")
-        return None, None
-
+#DB
 studentID=["A24AI0033","A24AI0044"]
 adminID = "Admin123"
-
-#Without function
-"""if login == 1:
-    student = str(input("Enter student ID :"))
-    #Student Valid ID
-    if student in studentID:
-        print("Welcome", student)
-    else:
-        print("Invalid student ID")
-elif login == 2:
-    admin = input("Enter admin ID :")
-    if admin in adminID:
-        print("Welcome", admin)
-    else:
-        print("Invalid admin ID")
-else:
-    print("Invalid Option")"""
-
-#Start Menu for Student
-#Student
-#-Add course
-#-Remove course
-#-View
 student_data = {}
 enrolled_subjects = []
 subjects = [
@@ -59,6 +10,32 @@ subjects = [
             {'code': 'DAT101', 'name': 'Data Management', 'credits': 3},
             {'code': 'IA101', 'name': 'Introduction to AI', 'credits': 2},
         ]
+#Login Student or Admin
+def login_admin_student():
+    while True:#Loop for invalid menu and ID
+        #Login Menu
+        print("Menu\n1.Student\n2.Admin")
+        signin = int(input("Select Option :"))
+        if signin == 1:  # Student login
+            student = input("Enter student ID: ").strip()
+            currentID = student
+            if student in studentID:
+                print("Welcome, Student:", student)
+                student_menu(currentID)
+            else:
+                print("Invalid student ID. Please try again.")
+
+        elif signin == 2:  # Admin login
+            admin = input("Enter admin ID: ").strip()
+            if admin == adminID:
+                print("Welcome, Admin:", admin)
+                admin_menu()
+            else:
+                print("Invalid admin ID. Please try again.")
+        else:
+            print("Invalid option. Please select 1 for Student or 2 for Admin.")
+
+#Student Menu
 def student_menu(currentID):
     total_credits = sum(sub["credits"] for sub in enrolled_subjects)
     max_credits = 12
@@ -142,67 +119,62 @@ def student_menu(currentID):
                 }
                 print(f"Data saved for {currentID}. Exiting...")
                 break
-        # Admin
-        # -Handle course
-        # -View report
-def admin_menu(currentID):
-        while True:
-            menu = int(input('Menu\n1.Manage Course\n2.View Students Enrollment\n3.Exit\nSelect Option :'))
-            if menu == 1:
-                print('The subjects available for this courses are:')
-                for subject in subjects:
-                    print(f'{subject["code"]}: {subject["name"]} ({subject["credits"]} credits)')
-                while True:
-                    action = input('1.Add Course\n2.Drop Course\n3.Exit\nSelect Option : ')
-                    if action == 1:
-                        sub_to_add = input('Enter subject name: ')
-                        code_to_add = input('Enter subject code: ')
-                        credit_to_add= int(input('Enter subject credits: '))
 
-                        if any(s['code'] == code_to_add for s in subjects):
-                            print(f"Subject with code {code_to_add} already exists.")
-                        else:
-                            new_subject = {"code": code_to_add, "name": sub_to_add, "credits": credit_to_add}
-                            subjects.append(new_subject)
-                            print(f"Subject {sub_to_add} added successfully!")
+#Admin Menu
+def admin_menu():
+    while True:
+        menu = int(input('Menu\n1.Manage Course\n2.View Students Enrollment\n3.Exit\nSelect Option :'))
+        if menu == 1:
+            print('The subjects available for this courses are:')
+            for subject in subjects:
+                print(f'{subject["code"]}: {subject["name"]} ({subject["credits"]} credits)')
+            while True:
+                action = input('1.Add Course\n2.Drop Course\n3.Exit\nSelect Option : ')
+                if action == 1:
+                    sub_to_add = input('Enter subject name: ')
+                    code_to_add = input('Enter subject code: ')
+                    credit_to_add = int(input('Enter subject credits: '))
 
-                    elif action == 2:
-                        code_to_remove = input('Enter subject code: ')
-                        sub_to_remove = None
-                        for s in subjects:
-                            if s['code'] == code_to_remove:
-                                sub_to_remove = s
-                                break
-                        if sub_to_remove:
-                            subjects.remove(sub_to_remove)
-                        else:
-                            print(f"Subject with code {code_to_remove} does not exist in the system.")
-
-                    elif action == 3:
-                        break
+                    if any(s['code'] == code_to_add for s in subjects):
+                        print(f"Subject with code {code_to_add} already exists.")
                     else:
-                        print('Invalid option.')
+                        new_subject = {"code": code_to_add, "name": sub_to_add, "credits": credit_to_add}
+                        subjects.append(new_subject)
+                        print(f"Subject {sub_to_add} added successfully!")
 
-            elif menu == 2:
+                elif action == 2:
+                    code_to_remove = input('Enter subject code: ')
+                    sub_to_remove = None
+                    for s in subjects:
+                        if s['code'] == code_to_remove:
+                            sub_to_remove = s
+                            break
+                    if sub_to_remove:
+                        subjects.remove(sub_to_remove)
+                    else:
+                        print(f"Subject with code {code_to_remove} does not exist in the system.")
+
+                elif action == 3:
+                    break
+                else:
+                    print('Invalid option.')
+
+            '''elif menu == 2:
                 print("Student Enrollment Data:")
                 for student_id, data in student_data.items():
-                    print(f"Student ID: {student_id}, Enrolled Subjects: {data['enrolled_subjects']}")
+                    print(f"Student ID: {student_id}\nEnrolled Subjects: ")
+                    for course in data:
+                          print(f"{course}")'''
+        elif menu == 2:
+            print("Student Enrollment Data:")
+            for student_id, data in student_data.items():
+                print(f"Student ID: \n{student_id}")
+                print(f"{'Code':<8}{'Name':<20}{'Credit':<6}")  #
+                print("-" * 35)
+                for subject in data['enrolled_subjects']:
+                    print(f"{subject['code']:<8}{subject['name']:<20}{subject['credits']:<6}")
 
-            else:
-                print('No such ID exists.')
+        else:
+            print('No such ID exists.')
 
-def enroll_sys():
-    # Choose Student or Admin
-    print("Menu\n1.Student\n2.Admin")
-    signin = int(input("Select Option :"))
-
-    # Verification#Current ID to hold ID key for add,remove,view.
-    role, currentID = login_admin_student(signin)
-    if role == 'student':
-        student_menu(currentID)
-    elif role == 'admin':
-        admin_menu(currentID)
-    else:
-        print("Invalid login details. Exiting...")
-
-enroll_sys()
+login_admin_student()
